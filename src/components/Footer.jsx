8f3a326e8df3bb363a7013e1f9c4fcb0f80113f7
özcan çetin <cetin445@gmail.com>
2022-08-12 23:42:46 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='p-4'>
+      <p className='text-center p-0 m-0 fs-6 fw-bold'>&#169; &nbsp;2022 <span className='fw-normal'>Compfy Sloth</span> All rights reserved</p>
+    </footer>
   )
 }
 
