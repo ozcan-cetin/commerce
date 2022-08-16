99706933644ff190576bcdcf68b8afa9cbc36d1b
özcan çetin <cetin445@gmail.com>
2022-08-16 11:12:20 +0300

    category v1

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,7 +6,12 @@ import { ProductContext } from "../context/ProductContext";
 import SingleProduct from "../components/SingleProduct";
 const Products = () => {
   const navigate = useNavigate();
-  const {products}=useContext(ProductContext)
+  const { products } = useContext(ProductContext);
+
+  const categories= ["All",...new Set(products.map((item)=>item.category))]
+
+  const company= ["All",...new Set(products.map((item)=>item.company))]
+
   console.log(products);
   return (
     <div>
@@ -16,25 +21,54 @@ const Products = () => {
           <span> / Products</span>
         </h1>
       </div>
-      <main className="container row m-auto mt-3 mt-md-5">
-        <div className="search col-2 bg-primary">jjjj</div>
+      <main className="container row m-auto mt-3 mt-md-5 p-0">
+
+        <div className="search col-2 bg-primary m-0 p-0">
+          <input type="search" placeholder="Search" className="px-1"/>
+          <ul className="category p-0 m-0">
+            <h6>Category</h6>
+{
+  categories.map((item)=>{
+    return(
+      <li className="text-capitalize list-unstyled">{item}</li>
+    )
+  })
+}
+          </ul>
+
+          <h6>Company</h6>
+          <select name="company" id="company">
+            {
+              company.map((item)=>{
+                return(
+                  <option value={item} className="text-capitalize">{item}</option>
+                )
+              })
+            }
+          </select>
+        </div>
+
         <div className="main-products col-10 m-0">
           <div className="main-products-upper d-flex align-items-center justify-content-between row">
             <div className="upper-btnDiv col-4 d-flex align-items-center justify-content-between">
               <div className="products-btnDiv d-flex align-items-center gap-1 p-0 m-0">
-                 <button className="d-flex align-items-center border-0 bg-transparent fs-4">
-                <FaBorderAll />{" "}
-              </button>
-              <button className="d-flex align-items-center border-0 bg-transparent fs-4">
-                <RiOrderPlayFill />{" "}
-              </button>
+                <button className="d-flex align-items-center border-0 bg-transparent fs-4">
+                  <FaBorderAll />{" "}
+                </button>
+                <button className="d-flex align-items-center border-0 bg-transparent fs-4">
+                  <RiOrderPlayFill />{" "}
+                </button>
               </div>
               <span>23 Products Found</span>
             </div>
             <div className="line-through border border-1 border-bottom border-dark col-5 "></div>
             <div className="col-3">
               <span className="me-2">Sort By</span>
-              <select name="select" id="select" className="border-0 bg-transparent" >
+              <select
+                name="select"
+                id="select"
+                className="border-0 bg-transparent"
+              >
                 <option value="Price(Lowest)">Price(Lowest)</option>
                 <option value="Price(Highest)">Price(Highest)</option>
                 <option value="Name(A-Z)">Name(A-Z)</option>
@@ -43,13 +77,9 @@ const Products = () => {
             </div>
           </div>
           <div className="main-products-bottom row">
-            {
-              products?.map((product)=>{
-                return(
-                  <SingleProduct key={product.id} product={product}/>
-                )
-              })
-            }
+            {products?.map((product) => {
+              return <SingleProduct key={product.id} product={product} />;
+            })}
           </div>
         </div>
       </main>
