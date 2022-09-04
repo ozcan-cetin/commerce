bac3f2931ed0422de3220f3975db53f74cfe4807
özcan çetin <cetin445@gmail.com>
2022-09-05 02:23:33 +0300

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login-div">
+      <form className="login-form m-auto  my-4 p-4 rounded-3">
+        <div className="mb-3">
+          <label htmlFor="firstName" className="form-label">
+            First Name
+          </label>
+          <input type="text" className="form-control" id="firstName" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="surname" className="form-label">
+            Surname
+          </label>
+          <input type="text" className="form-control" id="surname" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary w-100">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
